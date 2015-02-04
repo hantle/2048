dfba1d9c6145425f9b65d4b0ffa721f2f738947d
sungbae <laborercode@naver.com>
2015-02-04 20:44:03 +0900

    Implement Pad merge

--- Classes/EdgePadMover.cpp
+++ Classes/EdgePadMover.cpp
@@ -4,15 +4,26 @@
 int EdgePadMover::moveLeft(Board *board)
 {
     int size = board->getSize();
+    int point = 0;
 
     for(int r = 0 ; r < size ; r++) {
         for(int c = 0 ; c < size ; c++) {
             NumPad pad1 = board->getNumPad(r, c);
             if(pad1.mNum != 0) {
-                // check collision
-                //
-
                 if(c == 0) continue;
+
+                // check collision
+                NumPad pad2 = board->getNumPad(r, c - 1);
+                if(pad2.mNum != 0 && pad2.mNum == pad1.mNum) 
+                {
+                    board->setNum(r, c - 1, pad1.mNum + pad2.mNum);
+                    board->setNum(r, c, 0);
+                    point += pad2.mNum;
+
+                    continue;
+                }
+
+                if(pad2.mNum != 0 && pad2.mNum != pad1.mNum) continue;
 
                 board->setNum(r, c - 1, pad1.mNum);
                 board->setNum(r, c, 0);
@@ -26,15 +37,26 @@ int EdgePadMover::moveLeft(Board *board)
 int EdgePadMover::moveRight(Board *board) 
 {
     int size = board->getSize();
+    int point = 0;
 
     for(int r = 0 ; r < size ; r++) {
         for(int c = size - 1 ; c >= 0 ; c--) {
             NumPad pad1 = board->getNumPad(r, c);
             if(pad1.mNum != 0) {
-                // check collision
-                //
-
                 if(c == size - 1) continue;
+
+                // check collision
+                NumPad pad2 = board->getNumPad(r, c + 1);
+                if(pad2.mNum != 0 && pad2.mNum == pad1.mNum) 
+                {
+                    board->setNum(r, c + 1, pad1.mNum + pad2.mNum);
+                    board->setNum(r, c, 0);
+                    point += pad2.mNum;
+
+                    continue;
+                }
+
+                if(pad2.mNum != 0 && pad2.mNum != pad1.mNum) continue;
 
                 board->setNum(r, c + 1, pad1.mNum);
                 board->setNum(r, c, 0);
@@ -42,21 +64,32 @@ int EdgePadMover::moveRight(Board *board)
         }
     }
 
-    return 0;
+    return point;
 };
 
 int EdgePadMover::moveUp(Board *board)
 {
     int size = board->getSize();
+    int point = 0;
 
     for(int c = 0 ; c < size ; c++) {
         for(int r = size - 1 ; r >= 0 ; r--) {
             NumPad pad1 = board->getNumPad(r, c);
             if(pad1.mNum != 0) {
-                // check collision
-                //
-
                 if(r == size - 1) continue;
+
+                // check collision
+                NumPad pad2 = board->getNumPad(r + 1, c);
+                if(pad2.mNum != 0 && pad2.mNum == pad1.mNum) 
+                {
+                    board->setNum(r + 1, c, pad1.mNum + pad2.mNum);
+                    board->setNum(r, c, 0);
+                    point += pad2.mNum;
+
+                    continue;
+                }
+
+                if(pad2.mNum != 0 && pad2.mNum != pad1.mNum) continue;
 
                 board->setNum(r + 1, c, pad1.mNum);
                 board->setNum(r, c, 0);
@@ -64,21 +97,32 @@ int EdgePadMover::moveUp(Board *board)
         }
     }
 
-    return 0;
+    return point;
 }
 
 int EdgePadMover::moveDown(Board *board)
 {
     int size = board->getSize();
+    int point = 0;
 
     for(int c = 0 ; c < size ; c++) {
         for(int r = 0 ; r < size ; r++) {
             NumPad pad1 = board->getNumPad(r, c);
             if(pad1.mNum != 0) {
-                // check collision
-                //
-
                 if(r == 0) continue;
+
+                // check collision
+                NumPad pad2 = board->getNumPad(r - 1, c);
+                if(pad2.mNum != 0 && pad2.mNum == pad1.mNum) 
+                {
+                    board->setNum(r - 1, c, pad1.mNum + pad2.mNum);
+                    board->setNum(r, c, 0);
+                    point += pad2.mNum;
+
+                    continue;
+                }
+
+                if(pad2.mNum != 0 && pad2.mNum != pad1.mNum) continue;
 
                 board->setNum(r - 1, c, pad1.mNum);
                 board->setNum(r, c, 0);
@@ -86,5 +130,5 @@ int EdgePadMover::moveDown(Board *board)
         }
     }
 
-    return 0;
+    return point;
 }
